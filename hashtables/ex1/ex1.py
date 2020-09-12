dbbf13ad3a9d311fd7e205226558a7d73db62443
EvidenceN <33920231+EvidenceN@users.noreply.github.com>
2020-09-12 10:39:47 -0700

    almost done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,35 @@
+from itertools import combinations 
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
 
-    return None
+    if len(weights) > 1:
+        # find the pair that adds to weight limit using python combinations
+        test = [pair for pair in combinations(weights, 2) if sum(pair) == limit]
+        list_pair = list(test[0])
+
+        # store the list as key's, and indexes as values
+        hashtable = {}
+        for index, value in enumerate(weights):
+            hashtable[value] = index
+        
+        # getting the indices for weight combinations
+
+        zero_index = hashtable[list_pair[0]]
+
+        one_index = hashtable[list_pair[1]]
+
+        # the index combination sorted
+
+        indices = sorted((zero_index, one_index), reverse = True)
+
+        return hashtable
+    else:
+        return None
+
+
+weights = [4, 4]
+print(get_indices_of_item_weights(weights, 5, 8))
---
+++ interview-questions.md
@@ -0,0 +1,12 @@
+## Interview Questions
+
+Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your answers beforehand.
+
+1. Hashing functions
+2. Collision resolution
+3. Performance of basic hash table operations
+4. Load factor
+5. Automatic resizing
+6. Various use cases for hash tables
+
+We expect you to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade.
